bd063ff5cd442d89bf8bf7c6626d61ac93d81d24
Daniel Trstansky <daniel.trstansky@nov.sk>
2024-02-28 09:35:50 +0100

    Dokončenie mobile + resp

--- scripts.js
+++ scripts.js
@@ -1,6 +1,19 @@
 document.addEventListener("DOMContentLoaded", function () {
   const links = document.querySelectorAll("nav ul li a");
+  const hamburgerIcon = document.getElementById("burger")
+  const mobileMenu = document.getElementById("mobile-menu")
+  const closeMobileIcon = document.getElementById("close-icon")
 
+  // Mobile menu
+  hamburgerIcon.addEventListener("click", function(){
+    mobileMenu.classList.add("show")  
+  })
+
+  closeMobileIcon.addEventListener("click", function(){
+    mobileMenu.classList.remove("show")
+  })
+
+  // Desktop menu
   links.forEach(function (link) {
     const id = link.getAttribute("data-menu");
     const menu = document.getElementById(id);
